346683e177118adbfe13c285a94a3ef1bbb94e46
MatheusB4 <matheus.barreira123@gmail.com>
2025-09-15 20:59:19 -0300

    Tirando o cflush que estava evictando a linha de cache. Diminuindo quantidade de medicoes

--- auto_threshold.c
+++ auto_threshold.c
@@ -6,7 +6,7 @@
 
 #define BLOCK 64
 
-#define L1_CACHE_SIZE 48*1024
+#define L1_CACHE_SIZE 512*1024          /*511: limite máximo até segfault*/
 #define L2_CACHE_SIZE 1280*1024
 #define L3_CACHE_SIZE 8192*1024
 
@@ -30,7 +30,6 @@ static inline uint64_t  probe_native(volatile uint8_t *adrs){
         "    lfence                \n"
         "    rdtsc                 \n"
         "    sub %%esi, %%eax      \n"
-        "    clflush (%1)          \n"
         "    mfence                \n"
         "    lfence                \n"
         : "=a" (time)
@@ -40,7 +39,7 @@ static inline uint64_t  probe_native(volatile uint8_t *adrs){
     return time;
 }
 
-static inline uint64_t  dummy_probe_native(volatile uint8_t *adrs){
+static inline uint64_t  dummy_probe_native(){
     volatile unsigned long time;
     asm __volatile__(
         "    mfence                \n"
@@ -51,37 +50,37 @@ static inline uint64_t  dummy_probe_native(volatile uint8_t *adrs){
         "    lfence                \n"
         "    rdtsc                 \n"
         "    sub %%esi, %%eax      \n"
-        "    clflush (%1)          \n"
         "    mfence                \n"
         "    lfence                \n"
         : "=a" (time)
-        : "c" (adrs)
-        : "%esi", "%edx"
+        :: "%esi", "%edx"
     );
     return time;
 }
 
 void get_latency_l1(){
     int mix_i;
+    int NUM_MEDICOES = 1000;
     volatile uint8_t* addr;
-    long long int latency[L1_CACHE_SIZE];
-    long long int latency_dummy[L1_CACHE_SIZE];
+    long long int latency[NUM_MEDICOES];
+    long long int latency_dummy[NUM_MEDICOES];
 
-    for (int i=0; i < L1_CACHE_SIZE; i++){
+    for (unsigned int i=0; i < NUM_MEDICOES; i++){
 	//mix_i = ((i * 167) + 13) % L1_CACHE_SIZE;
-        latency[i] = probe_native(&array1[i]); /* MEMORY ACCESS TO TIME */
+	//printf("endereco: %p \n", &array1[i]);
+        latency[i] = probe_native(&array1[0]); /* MEMORY ACCESS TO TIME */
     }
-    for (int i=0; i < L1_CACHE_SIZE; i++){
+    for (unsigned int i=0; i < NUM_MEDICOES; i++){
 	//mix_i = ((i * 167) + 13) % L1_CACHE_SIZE;
-        latency_dummy[i] = dummy_probe_native(&array1[i]); /* MEMORY ACCESS TO TIME */
+        latency_dummy[i] = dummy_probe_native(); /* MEMORY ACCESS TO TIME */
     }
     
     unsigned int total_latency = 0;
     unsigned int subtracted_total_latency = 0;
-    for (int i = 0; i < L1_CACHE_SIZE; i++){total_latency += latency[i];}
-    for (int i = 0; i < L1_CACHE_SIZE; i++){subtracted_total_latency += (latency[i] - latency_dummy[i]);}
-    printf("cycles = %f \n", ((float)(total_latency))/((float)L1_CACHE_SIZE));
-    printf("cycles = %f \n", ((float)(subtracted_total_latency))/((float)L1_CACHE_SIZE));
+    for (long int i = 0; i < NUM_MEDICOES; i++){total_latency += latency[i];}
+    for (long int i = 0; i < NUM_MEDICOES; i++){subtracted_total_latency += (latency[i] - latency_dummy[i]);}
+    printf("cycles = %f \n", ((float)(total_latency))/((float)NUM_MEDICOES));
+    printf("cycles = %f \n", ((float)(subtracted_total_latency))/((float)NUM_MEDICOES));
 }
 
 

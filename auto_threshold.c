3d67dceeb2ec84b10c8a6ab65570ef8df5bf3ea6
MatheusB4 <matheus.barreira123@gmail.com>
2025-09-15 23:15:09 -0300

    função para fill de L1, medição de L2 e copy on write

--- auto_threshold.c
+++ auto_threshold.c
@@ -6,7 +6,7 @@
 
 #define BLOCK 64
 
-#define L1_CACHE_SIZE 512*1024          /*511: limite máximo até segfault*/
+#define L1_CACHE_SIZE 48*1024          /*511: limite máximo até segfault*/
 #define L2_CACHE_SIZE 1280*1024
 #define L3_CACHE_SIZE 8192*1024
 
@@ -58,6 +58,21 @@ static inline uint64_t  dummy_probe_native(){
     return time;
 }
 
+unsigned int max(long long int a, long long int b){
+    if(a>b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+
+void fill_array_l1(uint8_t *array){
+    for(unsigned int i = 0; i < L1_CACHE_SIZE/(4*1024); i++){
+        array[i*(4*1024)] = "m";
+    }
+}
+
 void get_latency_l1(){
     int mix_i;
     int NUM_MEDICOES = 1000;
@@ -78,12 +93,35 @@ void get_latency_l1(){
     unsigned int total_latency = 0;
     unsigned int subtracted_total_latency = 0;
     for (long int i = 0; i < NUM_MEDICOES; i++){total_latency += latency[i];}
-    for (long int i = 0; i < NUM_MEDICOES; i++){subtracted_total_latency += (latency[i] - latency_dummy[i]);}
+    for (long int i = 0; i < NUM_MEDICOES; i++){subtracted_total_latency += max(latency[i] - latency_dummy[i], 0);}
     printf("cycles = %f \n", ((float)(total_latency))/((float)NUM_MEDICOES));
     printf("cycles = %f \n", ((float)(subtracted_total_latency))/((float)NUM_MEDICOES));
 }
 
+void get_latency_l2(){
+   int NUM_MEDICOES = 1000;
+   volatile uint8_t lixo;
+   int total_latency = 0;
+   int total_latency_dummy = 0; 
+   int t1, t2 = 0;
+
+   for(int i = 0; i < NUM_MEDICOES; i++){
+       lixo = array1[0];
+       for(int j = 0; j < L1_CACHE_SIZE/64; j++){
+           lixo = array1[j*64];
+       }
+       t1 = probe_native(&array1[0]);
+       t2 = dummy_probe_native();
+       total_latency += t1;
+       total_latency_dummy += t2;
+       printf("t1 = %i, t2 = %i, t1 - t2: %i \n", t1, t2, t1 - t2);
+   }
+   printf("cycles = %f \n", ((float)(total_latency))/((float)NUM_MEDICOES));
+   printf("cycles = %f \n", ((float)(total_latency - total_latency_dummy))/((float)NUM_MEDICOES));
+}
 
 int main(){
     get_latency_l1();
+    fill_array_l1(array1);
+    get_latency_l2();
 }

40bf3371bd3cf29010ee214277219e546ee732d6
MatheusB4 <matheus.barreira123@gmail.com>
2025-09-19 16:35:50 -0300

    increased bandwidth_adjust of violin plot

--- plotter_violin.py
+++ plotter_violin.py
@@ -21,7 +21,7 @@ limite_superior = Q3 + 1.5 * IQR
 df_sem_outliers = df[(df["valor"] >= limite_inferior) & (df["valor"] <= limite_superior)]
 
 # Cria o violin plot
-a = sns.violinplot(y=df_sem_outliers["valor"], inner="box", color="r")  # inner="box" adiciona um boxplot dentro do violino
+a = sns.violinplot(y=df_sem_outliers["valor"], inner="box", color="r", bw_adjust=5)  # inner="box" adiciona um boxplot dentro do violino
 
 for collection in a.collections:
     collection.set_alpha(0.5) 
@@ -44,10 +44,34 @@ limite_superior = Q3 + 1.5 * IQR
 df_sem_outliers = df[(df["valor"] >= limite_inferior) & (df["valor"] <= limite_superior)]
 
 # Cria o violin plot
-b = sns.violinplot(y=df_sem_outliers["valor"], inner="box", color="b")  # inner="box" adiciona um boxplot dentro do violino
+b = sns.violinplot(y=df_sem_outliers["valor"], inner="box", color="b", bw_adjust=5)  # inner="box" adiciona um boxplot dentro do violino
 
 for collection in b.collections:
     collection.set_alpha(0.5) 
+
+#-----------------------------------Plota o grafico de violino para l2----------------------------------------#
+
+# Lê o CSV (um valor por linha)
+df = pd.read_csv("log_auto_threshold_l3", header=None, names=["valor"])
+
+# Calcula Q1, Q3 e IQR
+Q1 = df["valor"].quantile(0.25)
+Q3 = df["valor"].quantile(0.75)
+IQR = Q3 - Q1
+
+# Define limites para outliers
+limite_inferior = Q1 - 1.5 * IQR
+limite_superior = Q3 + 1.5 * IQR
+
+# Remove outliers
+df_sem_outliers = df[(df["valor"] >= limite_inferior) & (df["valor"] <= limite_superior)]
+
+# Cria o violin plot
+b = sns.violinplot(y=df_sem_outliers["valor"], inner="box", color="g", bw_adjust=5)  # inner="box" adiciona um boxplot dentro do violino
+
+for collection in b.collections:
+    collection.set_alpha(0.5) 
+
 
 
 plt.title("Violin Plot dos Valores")

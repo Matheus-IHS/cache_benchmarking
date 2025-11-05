84a276f2826d26efd0ade19613585cd8672bd7bc
MatheusB4 <matheus.barreira123@gmail.com>
2025-11-05 10:55:14 -0300

    Fixed probing address

--- auto_threshold_polished.c
+++ auto_threshold_polished.c
@@ -16,7 +16,7 @@ uint8_t array2[2*L2_CACHE_SIZE];
 
 
 static inline int probe_native(volatile uint8_t *adrs){
-    volatile unsigned long time;
+    volatile int time;
     asm __volatile__(
         "    mfence                \n"
         "    lfence                \n"
@@ -37,7 +37,7 @@ static inline int probe_native(volatile uint8_t *adrs){
 }
 
 static inline int  dummy_probe_native(){
-    volatile unsigned long time;
+    volatile int long time;
     asm __volatile__(
         "    mfence                \n"
         "    lfence                \n"
@@ -95,7 +95,7 @@ void get_latency_l1(){
     int mix_i;
     int NUM_MEDICOES = 500000;
     volatile uint8_t* addr;
-    unsigned latency[NUM_MEDICOES], latency_dummy[NUM_MEDICOES];
+    int latency[NUM_MEDICOES], latency_dummy[NUM_MEDICOES];
     FILE *LATENCY_FILE;
 
     for (int i=0; i < NUM_MEDICOES; i++){
@@ -117,31 +117,31 @@ void get_latency_l1(){
     fclose(LATENCY_FILE);
 }
 
-void get_latency_l2(){
-    int NUM_MEDICOES = 500000;
+void get_latency_l2(int num_medicoes, uint8_t *cache_filling_array, int array_size, char *log_file_name){
     volatile uint8_t lixo;
-    unsigned latency[NUM_MEDICOES], latency_dummy[NUM_MEDICOES];
-    unsigned total_latency = 0;
-    unsigned total_latency_dummy = 0;
-    unsigned subtracted_total_latency = 0;
+    uint8_t probe_address[1];
+    int latency[num_medicoes], latency_dummy[num_medicoes];
+    int total_latency = 0;
+    int total_latency_dummy = 0;
+    int subtracted_total_latency = 0;
     FILE *LATENCY_FILE;
 
-    for(int i = 0; i < NUM_MEDICOES; i++){
-        lixo = array2[0];
-        for(int j = 0; j < (2*L1_CACHE_SIZE)/64; j++){
+    for(int i = 0; i < num_medicoes; i++){
+        lixo = probe_address[0];
+        for(int j = 0; j < (array_size)/64; j++){
             lixo = array1[j*64];
         }
-        latency[i] = probe_native(&array2[0]);
+        latency[i] = probe_native(&probe_address[0]);
         latency_dummy[i] = dummy_probe_native();
     }
 
-    for (long int i = 0; i < NUM_MEDICOES; i++){total_latency += latency[i];}
-    for (long int i = 0; i < NUM_MEDICOES; i++){subtracted_total_latency += max(latency[i] - latency_dummy[i], 0);}
-    printf("cycles = %f \n", ((float)(total_latency))/((float)NUM_MEDICOES));
-    printf("cycles = %f \n", ((float)(subtracted_total_latency))/((float)NUM_MEDICOES));
+    for (long int i = 0; i < num_medicoes; i++){total_latency += latency[i];}
+    for (long int i = 0; i < num_medicoes; i++){subtracted_total_latency += max(latency[i] - latency_dummy[i], 0);}
+    printf("cycles = %f \n", ((float)(total_latency))/((float)num_medicoes));
+    printf("cycles = %f \n", ((float)(subtracted_total_latency))/((float)num_medicoes));
     
-    LATENCY_FILE = fopen("log_auto_threshold_l2", "w");
-    for(int i = 0; i < NUM_MEDICOES; i++){
+    LATENCY_FILE = fopen(log_file_name, "w");
+    for(int i = 0; i < num_medicoes; i++){
         fprintf(LATENCY_FILE, "%i\n", max(latency[i] - latency_dummy[i],0));
     }
     fclose(LATENCY_FILE);
@@ -150,12 +150,12 @@ void get_latency_l2(){
 void get_latency_l3(){
     int NUM_MEDICOES = 500000;
     volatile uint8_t lixo;
-    unsigned latency[NUM_MEDICOES];
-    unsigned latency_dummy[NUM_MEDICOES];
-    unsigned total_latency = 0;
-    unsigned total_latency_dummy = 0;
-    unsigned subtracted_total_latency = 0;
-    unsigned t1, t2 = 0;
+    int latency[NUM_MEDICOES];
+    int latency_dummy[NUM_MEDICOES];
+    int total_latency = 0;
+    int total_latency_dummy = 0;
+    int subtracted_total_latency = 0;
+    int t1, t2 = 0;
     FILE *LATENCY_FILE;
     
     for(int i = 0; i < NUM_MEDICOES; i++){
@@ -183,7 +183,7 @@ int main(){
     printf("long int: %lu \n", sizeof(long int));
     get_latency_l1();
     fill_array_l1(array1);
-    get_latency_l2();
+    get_latency_l2(500000, array1, 2*L1_CACHE_SIZE, "log_auto_threshold_l2");
     fill_array_l2(array2);
-    get_latency_l3();
+    get_latency_l2(500000, array2, 2*L2_CACHE_SIZE, "log_auto_threshold_l3");
 }
